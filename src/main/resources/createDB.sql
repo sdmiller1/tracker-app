430b61b31ca6cd7780abd82f16932bab1c577473
Sam Miller <sdmiller1@madisoncollege.edu>
2020-02-19 16:53:53 -0600

    worked on the database scripts

--- src/main/resources/createDB.sql
+++ src/main/resources/createDB.sql
@@ -1,14 +1,23 @@
+
+-- create the users table
+drop table if exists users;
+
 create table users (
     id int not null auto_increment,
     username varchar(255),
     password varchar(255),
     primary key (id)
-)
+);
+
+
+-- create the ratings table
+drop table if exists ratings;
 
 create table ratings (
     id int not null auto_increment,
     user_id int,
+    movie varchar(255),
     rating int,
     primary key id,
     foreign key user_id references users.id
-)
+);
---
+++ src/test/resources/cleanDB.sql
@@ -0,0 +1,36 @@
+
+-- create the users table
+drop table if exists users;
+
+create table users (
+    id int not null auto_increment,
+    username varchar(255),
+    password varchar(255),
+    primary key (id)
+);
+
+
+-- create the ratings table
+drop table if exists ratings;
+
+create table ratings (
+    id int not null auto_increment,
+    user_id int,
+    movie varchar(255),
+    rating int,
+    primary key id,
+    foreign key user_id references users.id
+);
+
+-- insert sample data
+
+insert into users values (default, 'bob', 'password');
+insert into users values (default, 'doug', 'password');
+insert into users values (default, 'chris', 'password');
+insert into users values (default, 'scott', 'password');
+
+insert into ratings values (default, 1, 'The Martian', 5);
+insert into ratings values (default, 2, 'The Martian', 5);
+insert into ratings values (default, 3, 'The Martian', 5);
+insert into ratings values (default, 4, 'The Martian', 5);
+insert into ratings values (default, 1, 'Harry Potter', 4);
