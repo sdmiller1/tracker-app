4ad876f1f66d85fdef7e1452dd4d91e1897e260f
Sam Miller <sdmiller1@madisoncollege.edu>
2020-02-23 14:29:32 -0600

    removed an unused import statement

--- src/main/java/com/movieapp/controller/RatingDao.java
+++ src/main/java/com/movieapp/controller/RatingDao.java
@@ -1,7 +1,6 @@
 package com.movieapp.controller;
 
 import com.movieapp.model.Rating;
-import com.movieapp.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
