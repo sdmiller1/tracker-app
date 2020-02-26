63abec1c93dcc58f8f3b50122ca8c658dd461f0f
Sam Miller <sdmiller1@madisoncollege.edu>
2020-02-26 17:21:15 -0600

    Implemented the new ERD into the sql scripts but it broke all of the tests which now need fixing

--- src/main/resources/createDB.sql
+++ src/main/resources/createDB.sql
@@ -1,23 +1,78 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2020-02-26 21:44:56.747
 
--- create the users table
-drop table if exists users;
-
-create table users (
-    id int not null auto_increment,
-    username varchar(255),
-    password varchar(255),
-    primary key (id)
+-- tables
+-- Table: Collections
+CREATE TABLE Collections (
+    id int NOT NULL AUTO_INCREMENT,
+    collectionType varchar(255) NOT NULL,
+    CONSTRAINT Collections_pk PRIMARY KEY (id)
 );
 
-
--- create the ratings table
-drop table if exists ratings;
-
-create table ratings (
-    id int not null auto_increment,
-    user_id int,
-    movie varchar(255),
-    rating int,
-    primary key id,
-    foreign key (user_id) references users(id)
+-- Table: Movies
+CREATE TABLE Movies (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    CONSTRAINT Movies_pk PRIMARY KEY (id)
 );
+
+-- Table: MoviesCollections
+CREATE TABLE MoviesCollections (
+    collection_id int NOT NULL,
+    movie_id int NOT NULL,
+    CONSTRAINT MoviesCollections_pk PRIMARY KEY (collection_id,movie_id)
+);
+
+-- Table: Ratings
+CREATE TABLE Ratings (
+    user_id int NOT NULL,
+    movie_id int NOT NULL,
+    dateWatched date NOT NULL,
+    rating int NOT NULL,
+    CONSTRAINT Ratings_pk PRIMARY KEY (user_id,movie_id,dateWatched)
+);
+
+-- Table: Users
+CREATE TABLE Users (
+    id int NOT NULL AUTO_INCREMENT,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    CONSTRAINT Users_pk PRIMARY KEY (id)
+);
+
+-- Table: UsersCollections
+CREATE TABLE UsersCollections (
+    user_id int NOT NULL,
+    collections_id int NOT NULL,
+    CONSTRAINT UsersCollections_pk PRIMARY KEY (user_id,collections_id)
+);
+
+-- foreign keys
+-- Reference: MoviesCollections_Collections (table: MoviesCollections)
+ALTER TABLE MoviesCollections ADD CONSTRAINT MoviesCollections_Collections FOREIGN KEY MoviesCollections_Collections (collection_id)
+    REFERENCES Collections (id);
+
+-- Reference: MoviesCollections_Movies (table: MoviesCollections)
+ALTER TABLE MoviesCollections ADD CONSTRAINT MoviesCollections_Movies FOREIGN KEY MoviesCollections_Movies (movie_id)
+    REFERENCES Movies (id);
+
+-- Reference: Ratings_Movies (table: Ratings)
+ALTER TABLE Ratings ADD CONSTRAINT Ratings_Movies FOREIGN KEY Ratings_Movies (movie_id)
+    REFERENCES Movies (id);
+
+-- Reference: Ratings_Users (table: Ratings)
+ALTER TABLE Ratings ADD CONSTRAINT Ratings_Users FOREIGN KEY Ratings_Users (user_id)
+    REFERENCES Users (id);
+
+-- Reference: UsersCollections_Collections (table: UsersCollections)
+ALTER TABLE UsersCollections ADD CONSTRAINT UsersCollections_Collections FOREIGN KEY UsersCollections_Collections (collections_id)
+    REFERENCES Collections (id);
+
+-- Reference: UsersCollections_Users (table: UsersCollections)
+ALTER TABLE UsersCollections ADD CONSTRAINT UsersCollections_Users FOREIGN KEY UsersCollections_Users (user_id)
+    REFERENCES Users (id);
+
+-- End of file.
+
--- src/test/resources/cleanDB.sql
+++ src/test/resources/cleanDB.sql
@@ -1,38 +1,105 @@
--- Drop tables in the correct order
-drop table if exists ratings;
-drop table if exists users;
+-- drop tables before re creating them
+drop table if exists MoviesCollections;
+drop table if exists Ratings;
+drop table if exists Movies;
+drop table if exists UsersCollections;
+drop table if exists Collections;
+drop table if exists Users;
 
 
--- create the users table
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2020-02-26 21:44:56.747
 
-create table users (
-    id int not null auto_increment,
-    username varchar(255),
-    password varchar(255),
-    primary key (id)
+-- tables
+-- Table: Collections
+CREATE TABLE Collections (
+    id int NOT NULL AUTO_INCREMENT,
+    collectionType varchar(255) NOT NULL,
+    CONSTRAINT Collections_pk PRIMARY KEY (id)
 );
 
-
--- create the ratings table
-
-create table ratings (
-    id int not null auto_increment,
-    user_id int,
-    movie varchar(255),
-    rating int,
-    primary key (id),
-    foreign key (user_id) references users(id) on delete cascade
+-- Table: Movies
+CREATE TABLE Movies (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    CONSTRAINT Movies_pk PRIMARY KEY (id)
 );
+
+-- Table: MoviesCollections
+CREATE TABLE MoviesCollections (
+    collection_id int NOT NULL,
+    movie_id int NOT NULL,
+    CONSTRAINT MoviesCollections_pk PRIMARY KEY (collection_id,movie_id)
+);
+
+-- Table: Ratings
+CREATE TABLE Ratings (
+    user_id int NOT NULL,
+    movie_id int NOT NULL,
+    dateWatched date NOT NULL,
+    rating int NOT NULL,
+    CONSTRAINT Ratings_pk PRIMARY KEY (user_id,movie_id,dateWatched)
+);
+
+-- Table: Users
+CREATE TABLE Users (
+    id int NOT NULL AUTO_INCREMENT,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    CONSTRAINT Users_pk PRIMARY KEY (id)
+);
+
+-- Table: UsersCollections
+CREATE TABLE UsersCollections (
+    user_id int NOT NULL,
+    collections_id int NOT NULL,
+    CONSTRAINT UsersCollections_pk PRIMARY KEY (user_id,collections_id)
+);
+
+-- foreign keys
+-- Reference: MoviesCollections_Collections (table: MoviesCollections)
+ALTER TABLE MoviesCollections ADD CONSTRAINT MoviesCollections_Collections FOREIGN KEY MoviesCollections_Collections (collection_id)
+    REFERENCES Collections (id);
+
+-- Reference: MoviesCollections_Movies (table: MoviesCollections)
+ALTER TABLE MoviesCollections ADD CONSTRAINT MoviesCollections_Movies FOREIGN KEY MoviesCollections_Movies (movie_id)
+    REFERENCES Movies (id);
+
+-- Reference: Ratings_Movies (table: Ratings)
+ALTER TABLE Ratings ADD CONSTRAINT Ratings_Movies FOREIGN KEY Ratings_Movies (movie_id)
+    REFERENCES Movies (id);
+
+-- Reference: Ratings_Users (table: Ratings)
+ALTER TABLE Ratings ADD CONSTRAINT Ratings_Users FOREIGN KEY Ratings_Users (user_id)
+    REFERENCES Users (id);
+
+-- Reference: UsersCollections_Collections (table: UsersCollections)
+ALTER TABLE UsersCollections ADD CONSTRAINT UsersCollections_Collections FOREIGN KEY UsersCollections_Collections (collections_id)
+    REFERENCES Collections (id);
+
+-- Reference: UsersCollections_Users (table: UsersCollections)
+ALTER TABLE UsersCollections ADD CONSTRAINT UsersCollections_Users FOREIGN KEY UsersCollections_Users (user_id)
+    REFERENCES Users (id);
+
+
+
+
 
 -- insert sample data
 
-insert into users values (default, 'bob', 'password');
-insert into users values (default, 'doug', 'password');
-insert into users values (default, 'chris', 'password');
-insert into users values (default, 'scott', 'password');
+insert into Users values (default, 'Bob', 'Behnken', 'astrobob', 'password');
+insert into Users values (default, 'Doug', 'Hurley', 'astrodoug', 'password');
+insert into Users values (default, 'Chris', 'Hadfield', 'astrochris', 'password');
+insert into Users values (default, 'Scott', 'Kelly', 'astroscott', 'password');
 
-insert into ratings values (default, 1, 'The Martian', 5);
-insert into ratings values (default, 2, 'The Martian', 5);
-insert into ratings values (default, 3, 'The Martian', 5);
-insert into ratings values (default, 4, 'The Martian', 5);
-insert into ratings values (default, 1, 'Harry Potter', 4);
+insert into Movies values (default, 'The Martian');
+insert into Movies values (default, 'Harry Potter');
+insert into Movies values (default, 'Ad Astra');
+
+insert into Ratings values (1, 1, 5);
+insert into Ratings values (2, 1, 5);
+insert into Ratings values (3, 1, 5);
+insert into Ratings values (4, 1, 5);
+insert into Ratings values (1, 1, 4);
