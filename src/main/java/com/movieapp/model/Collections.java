57b5742695d662e11bcfe6c4ac4b09be5cbf4f1c
Sam Miller <sdmiller1@madisoncollege.edu>
2020-02-27 16:59:03 -0600

    created the collections data object

---
+++ src/main/java/com/movieapp/model/Collections.java
@@ -0,0 +1,84 @@
+package com.movieapp.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity(name = "Collections")
+@Table(name = "Collections")
+public class Collections {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+
+    @Column(name = "collectionType")
+    private String collectionType;
+
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "UsersCollections",
+            joinColumns = { @JoinColumn(name = "collection_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    Set<User> users = new HashSet<>();
+
+    public Collections() {
+
+    }
+
+    public Collections(String collectionType, Set<User> users) {
+        this.collectionType = collectionType;
+        this.users = users;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCollectionType() {
+        return collectionType;
+    }
+
+    public void setCollectionType(String collectionType) {
+        this.collectionType = collectionType;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Collections that = (Collections) o;
+        return id == that.id &&
+                Objects.equals(collectionType, that.collectionType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, collectionType);
+    }
+
+    @Override
+    public String toString() {
+        return "Collections{" +
+                "id=" + id +
+                ", collectionType='" + collectionType + '\'' +
+                '}';
+    }
+}
