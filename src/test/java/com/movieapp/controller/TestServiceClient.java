d9e389eb020c6e6e8be5b91f3b6167a1387e7d3c
Sam Miller <sdmiller1@madisoncollege.edu>
2020-03-26 12:42:50 -0500

    added a logger for testing

--- src/test/java/com/movieapp/controller/TestServiceClient.java
+++ src/test/java/com/movieapp/controller/TestServiceClient.java
@@ -2,6 +2,8 @@ package com.movieapp.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.omdb.Movie;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
@@ -9,9 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestServiceClient {
 
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
     @Test
     public void testOMDBJSON() throws Exception {
         Movie movie = new OMDBDao().getMovieByTitle("The Martian");
+
+        logger.error(movie);
 
         assertEquals("The Martian", movie.getTitle());
     }
